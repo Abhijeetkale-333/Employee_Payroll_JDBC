0236b29ae2196e37ee9fdf71fe0de0cc83627519
Your Name <you@example.com>
2021-05-25 01:35:17 +0530

    added:MySQL connector Dependency to Project and Established a connection

---
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmployeePayrollTest {
+    //Rigorous Test :-)
+
+        @Test
+        public void shouldAnswerWithTrue ()
+        {
+            assertTrue(true);
+        }
+}
